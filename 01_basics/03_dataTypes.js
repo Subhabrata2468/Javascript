14d50b6608ef2569dd5895c547645edae5c9cef0
SUBHABRATA PANDA <126705074+Subhabrata2468@users.noreply.github.com>
2024-03-30 21:23:23 +0000

    dataTypes

---
+++ 01_basics/03_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict";  // treat all JS code as newer version
+
+// alert (3+3) // we are using nodejs , not brower
+
+console.log(3 
+    +
+    3) // code readabliltty should be high
+
+let name = "devil" //string
+let age = 18  // int 
+let bool = true // boolean
+
+
+//numer/int =>> 2 to pow 53
+//bigint
+//string =>> ""
+// boolean =>> true/false
+//null =>> standalone value
+//sumbol =>> unique
+
+//object
+
+console.log(typeof "Yono") //string
+console.log(typeof undefined) // undifined
+console.log(typeof null) //object
