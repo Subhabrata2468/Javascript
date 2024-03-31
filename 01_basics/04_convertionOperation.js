177d090d03916e6d88175030b15a61ca38e7f048
SUBHABRATA PANDA <126705074+Subhabrata2468@users.noreply.github.com>
2024-03-31 14:24:09 +0000

    convertionOperation part2

--- 01_basics/04_convertionOperation.js
+++ 01_basics/04_convertionOperation.js
@@ -32,3 +32,31 @@ let trial= true
  let booleanLoggedIn1 = String(loggedIn1)
  console.log(booleanLoggedIn1)
  console.log(typeof booleanLoggedIn1)
+
+
+ // **************************** Operation ****************************
+
+ let val = 3
+ let negeVal = -val
+ console.log (negeVal)
+
+ let str1 = "Demon"
+ let str2 = "King" 
+ console.log(str1+str2)
+
+ console.log("1" + 2) //12
+ console.log(1+"2") //12
+ console.log("1"+2+2) //122
+ console.log(2+2+"1") //41
+ console.log((3+4)*5%3) //2
+ console.log(+true)
+ console.log(+"")
+
+ let num1,num2,num3
+ num1=num2=num3=2+2
+
+ let counter=100
+ ++counter //pre
+ console.log(counter)
+ counter++ //post
+ console.log(counter)
