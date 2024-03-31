96f83a6b4f6d563cbd56d1bb938c60a6aa817fee
SUBHABRATA PANDA <126705074+Subhabrata2468@users.noreply.github.com>
2024-03-31 14:36:11 +0000

    comparision

---
+++ 01_basics/05_comparision.js
@@ -0,0 +1,12 @@
+// to compare it must have same datatype
+
+console.log("2" >1)
+console.log("2" >1)
+
+console.log(null > 0) //false
+console.log(null == 0) //false
+console.log(null >= 0)  //true
+
+//===
+console.log("2"===2)
+
