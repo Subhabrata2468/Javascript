1fae8268f8a8216142732cae7edddd6b81683d3d
SUBHABRATA PANDA <126705074+Subhabrata2468@users.noreply.github.com>
2024-03-31 14:08:40 +0000

    convertionOperation part 1

---
+++ 01_basics/04_convertionOperation.js
@@ -0,0 +1,34 @@
+ let score= "33as"
+
+ console.log(typeof score)
+ console.log(typeof (score))
+
+ //typeCasting
+ let convertNumber= Number(score)
+ console.log(typeof convertNumber)
+ console.log(convertNumber)
+
+ //"33" => 33
+// "33acs" => NaN
+//true => 1 && false => 0
+//"" => false
+//"DEvil" => true
+
+let trial= true
+
+ console.log(typeof trial)
+ console.log(typeof (trial))
+
+ //typeCasting
+ let convertNumber2= Number(trial)
+ console.log(typeof convertNumber2)
+ console.log(convertNumber2)
+
+ let loggedIn = 1 
+ let booleanLoggedIn = Boolean(loggedIn)
+ console.log(booleanLoggedIn)
+
+ let loggedIn1 = 33
+ let booleanLoggedIn1 = String(loggedIn1)
+ console.log(booleanLoggedIn1)
+ console.log(typeof booleanLoggedIn1)
